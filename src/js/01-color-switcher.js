b440108a5b10ef3cd570849cb1b99e654e7b879a
Maryna Skoryk <skoryk.maryna@gmail.com>
2022-11-27 12:33:19 +0100

    done 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startBtn: document.querySelector('button[data-start'),
+  stopBtn: document.querySelector('button[data-stop'),
+};
+
+let intervalID = null;
+
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  intervalID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(intervalID);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
